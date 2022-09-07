bb0f15b6c4555ceb57baa7cac34b1045139c0e88
jeongdxxn <dxxn.jeong@gmail.com>
2022-09-07 18:17:16 +0900

    Set the tailwind and create a header

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cbbf52c3a78777fa"; }
+/******/ 		__webpack_require__.h = function() { return "8c133ad05592eef3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header>
+      <h1 className="text-sky-500 dark:text-sky-400 w-100 h-50">
+        Welcome to Lighting Catalog!
+      </h1>
+    </header>
+  )
+}
