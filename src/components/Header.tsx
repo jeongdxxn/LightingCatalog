1c8225f175309e36f0df8fe7d9ffcae5f8fe6fa2
jeongdxxn <dxxn.jeong@gmail.com>
2022-10-25 17:45:54 +0900

    fix: header layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,21 @@
+import Image from 'next/image'
+import sa_lighting_logo from '../../public/images/sa_lighting_logo.png'
+
 export default function Header() {
   return (
-    <header>
-      <h1 className="text-sky-500 dark:text-sky-400 w-100 h-50">
-        Welcome to Lighting Catalog!
-      </h1>
+    <header className="w-full flex items-center justify-between p-6">
+      <Image src={sa_lighting_logo} alt="logo" width={250} height={50} />
+      <nav>
+        <ul className="flex space-x-4 cursor-pointer">
+          <li>전체</li>
+          <li>식탁/펜던트</li>
+          <li>방등</li>
+          <li>거실등</li>
+          <li>벽등</li>
+          <li>주방</li>
+          <li>직부/센서</li>
+        </ul>
+      </nav>
     </header>
   )
 }
